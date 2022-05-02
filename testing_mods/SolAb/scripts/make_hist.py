119752277f630965a6c9bd81a80976972fb42a70
Evelyn Annbelle Fuhrman <fuhrmane@msu.edu>
2022-05-02 18:41:07 -0400

    that do be all, folks :')

--- testing_mods/SolAb/scripts/make_hist.py
+++ testing_mods/SolAb/scripts/make_hist.py
@@ -1,35 +1,37 @@
 import pandas as pd
-import matplotlib.pylot as plt
-import os
-import sys
-import argparse
+import matplotlib.pyplot as plt 
+import numpy as np
 
-parser = argparse.ArgumentParser(description = "args for sorting script")
-parser.add_argument('--ds', nargs='?', action='store', required=True, dest='path', help='Path to absorber catalogs. The program will create a directory here to store its generated txt files and png files')
-parser.add_argument('--il', nargs='?', action='store', required=True, dest='interval_list', help='2D list of interval ranges')
-parser.add_argument('--fn', nargs='?', action='store', required=True, dest='filename', help='txt file of clump data extacted in plot_clumps.py')
-args = parser.parse_args()
-dic_args = vars(args)
+# Takes path to where SALSA abunance catalogs are stored and ion list used in generating them to extract necessary columnn density data into a big boi dictionary
+path = "/mnt/home/fuhrmane/pipeline_tests/hist_test/data/" # modify as needed
+row_dictionary = {}
+coldens_dic = {}
+ion_list = ["C_II", "O_VI", "C_IV"] # modify as needed
+for ion in ion_list:
+    row_dictionary[f'{ion}'] = {}
+    coldens_dic[f'{ion}'] = {}
+    for i in range(26):
+        row_dictionary[f'{ion}'][f'row{i+1}'] = {}
+        row_data = pd.read_csv(path+f"data_AbundanceRow{i}_{ion}.txt", delim_whitespace=True)
+        for ray in range(4):
+            row_dictionary[f'{ion}'][f'row{i+1}'][f'ray_index{ray}'] = row_data[row_data['lightray_index']==ray].reset_index()
+            # row_dictionary[f'{ion}'][f'row{i+1}'][f'ray_index{ray}'].reset_index()
+            coldens_dic[f'{ion}'][f'column_densities{ray}'] = np.zeros(26)
 
-directory_path = os.path.expandvars(os.path.expanduser(args.path))
-clump_dir = directory_path+"clump_plots3"
+# Somewhat automatically -- when inteerval chunks line up exactly. Use the "ray", "ion", and "interval" variables instead of modifying the for loop
+ray = "1"
+ion = ion_list[1]
+interval = 0
+for i in range(26):
+    row = row_dictionary[ion][f'row{i+1}']
+    coldens_dic[ion][f'column_densities{ray}'][i] += row_dictionary[ion][f'row{i+1}'][f'ray_index{ray}']["col_dens"][interval]
+plt.hist(coldens_dic[ion][f'column_densities{ray}'])
+plt.title(f"{ion} -- LightRay Index {ray} -- Interval {str(interval)}")
+plt.savefig(f"/mnt/home/fuhrmane/pipeline_tests/hist_test/data/hist_clumps/Hist_{ion}_RayIndex{ray}_Int{interval}.png")
 
-data = pd.read_csv(args.filename, delim_whitespace=True)
-ion_position = args.filename.find('.')-4
-ion = args.filename[ion_position]
-for i in range(ion_position+1, ion_position+4):
-    ion += agrs.filename[i]
+# Manually -- for when interval chunks for rows don't exactly line up
+coldens_dic[ion][f'column_densities{ray}'][0] += row_dictionary[ion][f'row1'][f'ray_index{ray}']["col_dens"][0]
 
-for row_num in range(data['row_number'].max()):
-    for ray_num in range(data['cell_index'].max()):
-        ray_and_row_data = data[data['cell_index']==ray_num & data['row_number']==row_num]
-        plt.hist(ray_and_row_data, bins=interval_list, stacked=True)
-        Title = f'{ion} -- Cell Index {ray_num}'
-    
-    plt.title(Title)
-    plt.xlabel('interval ranges')
-    plt.ylabel('column densities')
-        # for  i in range(len(ray_and_row_data)):
-        #     for int in args.interval_list:
-        #         if (ray_and_row_data['interval_start'][i] >= int[0] & ray_and_row_data['interval_end'] <= int[1]):
-        #             # add to bin
+plt.hist(coldens_dic[ion][f'column_densities{ray}'])
+plt.title(f"{ion} -- LightRay Index {ray} -- Interval {str(interval)}")
+plt.savefig(f"/mnt/home/fuhrmane/pipeline_tests/hist_test/data/hist_clumps/Hist_{ion}}_RayIndex{ray}_Int{str(interval)}.png")
--- testing_mods/SolAb/scripts/plot_clumps.py
+++ testing_mods/SolAb/scripts/plot_clumps.py
@@ -14,7 +14,7 @@ dic_args = vars(args)
 
 directory_path = os.path.expandvars(os.path.expanduser(args.path))
 # print(f"DIRECTORY_PATH: {directory_path}")
-new_dir = "clump_plots3"
+new_dir = "hist_clumps"
 if not os.path.exists(directory_path+f"{new_dir}"):
     # os.makedirs(dirctory_path)
     os.mkdir(directory_path+f"{new_dir}")
@@ -36,22 +36,22 @@ for i in range(1, len(num_ticks)-1):
     tick_labels.append(f'row {i}')
 tick_labels.append(' ')
 
-master_dic = {}
-master_dic[f'row_number'] = []
-master_dic[f'cell_index'] = []
-master_dic[f'interval_start'] = []
-master_dic[f'interval_end'] = []
-master_dic[f'column_density'] = []
+# master_dic = {}
+# master_dic[f'row_number'] = []
+# master_dic[f'cell_index'] = []
+# master_dic[f'interval_start'] = []
+# master_dic[f'interval_end'] = []
+# master_dic[f'column_density'] = []
 
 for ray_num in range(int(preliminary_dummy_data['lightray_index'].max())):
     for i in range(len(files)):
         data = pd.read_csv(files[i], delim_whitespace=True)
         ray_data = data[data['lightray_index']==ray_num]
-        master_dic[f'row_number'].extend(np.ones(len(ray_data))*ray_num)
-        master_dic[f'cell_index'].extend(ray_data)
-        master_dic[f'interval_start'].extend(data[data['interval_start'] == ray_num])
-        master_dic[f'interval_end'].extend(data[data['interval_end'] == ray_num])
-        master_dic[f'column_density'].extend(data[data['column_density'] == ray_num])
+        # master_dic[f'row_number'].extend(np.ones(len(ray_data))*ray_num)
+        # master_dic[f'cell_index'].extend(ray_data)
+        # master_dic[f'interval_start'].extend(data[data['interval_start'] == ray_num])
+        # master_dic[f'interval_end'].extend(data[data['interval_end'] == ray_num])
+        # master_dic[f'column_density'].extend(data[data['col_dens'] == ray_num])
         # print(f"for {ion} and cell index {ray_num}: adding row {i}...")
         plt.hlines(np.ones(ray_data.shape[0])*num_ticks[i+1], ray_data['interval_start'], ray_data['interval_end'])
         plt.title(f"{ion} -- Cell Index {ray_num}")
@@ -59,14 +59,14 @@ for ray_num in range(int(preliminary_dummy_data['lightray_index'].max())):
         plt.yticks(num_ticks, tick_labels)
     
 
-    plt.savefig(directory_path+f"clump_plots3/ClumpPlot_{ion}_RayIndex{ray_num}.png")
+    plt.savefig(directory_path+f"{new_dir}/ClumpPlot_{ion}_RayIndex{ray_num}.png")
 
-if not os.path.exists(directory_path+f"/{new_dir}/master_{ion}.txt"):
-    # how to open without creating?
-    master = open(directory_path+f"/{new_dir}/master_{ion}.txt", "w")
-else:
-    master = directory_path + f"/{new_dir}/master_{ion}.txt"
+# if not os.path.exists(directory_path+f"/{new_dir}/master_{ion}.txt"):
+#     # how to open without creating?
+#     master = open(directory_path+f"/{new_dir}/master_{ion}.txt", "w")
+# else:
+#     master = directory_path + f"/{new_dir}/master_{ion}.txt"
 
-master_df = pd.DataFrame.from_dict(master_dic)
-master.write(master_df)
-master.close() 
+# master_df = pd.DataFrame.from_dict(master_dic)
+# master.write(master_df)
+# master.close() 
