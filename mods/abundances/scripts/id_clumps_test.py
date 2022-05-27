62ea5c6e376623d5df810c9e564c6894b6514a19
amrollins <102754385+amrollins@users.noreply.github.com>
2022-05-27 14:30:18 -0400

    added comments

--- mods/abundances/scripts/id_clumps_test.py
+++ mods/abundances/scripts/id_clumps_test.py
@@ -13,12 +13,12 @@ df1_work=df1[df1["lightray_index"]==1] ##filter to only ray1
 df2_work=df2[df2["lightray_index"]==1]
 df1_clumps = df1_work[["interval_start","interval_end"]] ##filter to only indexes
 df2_clumps = df2_work[["interval_start","interval_end"]]
-df1_st = np.asarray(df1_clumps["interval_start"]) ##make each interval start and end into lists for easy indexing
-df2_st = np.asarray(df2_clumps["interval_start"])
-df1_en = np.asarray(df1_clumps["interval_end"])
-df2_en = np.asarray(df2_clumps["interval_end"])
+# df1_st = np.asarray(df1_clumps["interval_start"]) ##make each interval start and end into lists for easy indexing
+# df2_st = np.asarray(df2_clumps["interval_start"])
+# df1_en = np.asarray(df1_clumps["interval_end"])
+# df2_en = np.asarray(df2_clumps["interval_end"])
 
-mx= -np.inf 
+mx= -np.inf  ##find how long each array should be
 rowlist = [df1_clumps, df2_clumps]
 for ds in rowlist: #find the cell index of the furthest clump
   row_mx = max(ds["interval end"])
@@ -28,112 +28,78 @@ for ds in rowlist: #find the cell index of the furthest clump
 super_clumps = np.zeros(int(mx))
 
 clmaps = []
-for ds in rowlist:
+for ds in rowlist: ##make masks for each row and form super_clumps
   ds_clump_loc = np.zeros(int(mx))
   for i in range(ds.shape[0]):
     ds_clump_loc[int(ds["interval_start"][i]):int(ds["interval_end"][i])] = 1
     super_clumps[int(ds["interval_start"][i]):int(ds["interval_end"][i])] = 1
   clmaps.append(ds_clump_loc)
-super_clumps.append(0, super_clumps)    
-
-one_locs = np.where(super_clumps == 1)[0]
-start_list = []
-end_list = []
-for i in range(len(one_locs)):
-  if one_locs[i+1] - one_locs[i] > 1:
-    start_list.append(one_locs[i+1])
-    end_list.append(one_locs[i])
+super_clumps=np.append(0, super_clumps)  ##make indexing work  
+super_clumps=np.append(super_clumps, 0)
                     
-match = {} ##create dictionaries to store indexes of clumps in the row that correspond to one another, keys will be row numbers and values will be indecies
+match = {} ##create dictionaries to store indexes of clumps in the row that correspond to one another, keys will be row numbers and values will be indecies except for the lonlies
 short = {}
 merge = {}
 lonely = {}
 maybe_lonely = {}
 
 rownum = 0
-for row in clmaps:
-  rownum += 1
-  row=np.append(0, row)
+for row in clmaps: ##start by iterating over a whole row
+  rownum += 1 ##define which row we're working on
+  row=np.append(0, row) ##make indexing work
+  row=np.append(row, 0)
   
-  row_st_count = 0
-  row_end_cnt = 0
-  row_st_ind = []
-  row_en_ind = []
+  ##make all the variables and lists necessary
+  row_st_count = 0 ##count how many starts of row clumps there have been within a super clump 
+  row_end_cnt = 0 ##how many ends of row clumps there have been within a super clump
+  row_st_ind = [] ##keep track of start location(s) of a row clump
+  row_en_ind = [] ##keep track of end location(s) of a row clump
   
   row_match=[]
   row_short = []
   row_merge = []
   
-  if super_clumps[i-1] < super_clumps[i]:
-    sp_st = i-1
+  if super_clumps[i-1] < super_clumps[i]: ##start of a super clump
+    sp_st = i-1 ##keep track of start location of a super clump
     
-  if row[i-1] < row[i]:
-    row_st_ind.append(i-1)
-    row_st_cnt += 1
+  if row[i-1] < row[i]: ##start of a clump in the row
+    row_st_ind.append(i-1) 
+    row_st_cnt += 1 
     
-  elif row[i-1] > row[i]:
-    row_en_ind.append(i-1)
-    row_en_cnt += 1
+  elif row[i-1] > row[i]: ##end of a clump in row
+    row_en_ind.append(i-1) 
+    row_en_cnt += 1 
   
-  if super_clumps[i-1] > super_clumps[i]:
-    sp_en = i-1
+  if super_clumps[i-1] > super_clumps[i]: ##end of a super clump
+    sp_en = i-1 ##keep track of the location of the end of a super clump
     
-    if (row_st_count == 1) and (row_en_cnt == 1):
-      if (row_st_ind[0] == sp_st) and (row_en_ind[0] == sp_en):
-        row_match.append([row_st_ind[0], row_en_ind[0]])
+    if (row_st_count == 1) and (row_en_cnt == 1): ##check for if there is only one row clump in the super clump
+      if (row_st_ind[0] == sp_st) and (row_en_ind[0] == sp_en): ##if the starts and ends match, the clumps are identical
+        row_match.append([row_st_ind[0], row_en_ind[0]]) ##thus, start and end indecies appended to a list of them
       else:
-        row_short.append([row_st_ind[0], row_end_ind[0]])
+        row_short.append([row_st_ind[0], row_end_ind[0]]) ##if not, then the row clump must be shorter and the start and end indecies are appended to the appropraite list
         
-    elif (row_st_cnt == 0) and (row_en_cnt ==0):
-      if str([sp_st, sp_en]) in maybe_lonely.keys():
+    elif (row_st_cnt == 0) and (row_en_cnt ==0): ##check if there is nothing in the row that matches the super clump
+      if str([sp_st, sp_en]) in maybe_lonely.keys(): ##check if we have already seen this superclump, if not make the entry in the dictionary
         maybe_lonely[str([sp_st, sp_en])] += 1
-      else:
+      else: 
         maybe_lonely[str([sp_st, sp_en])] = 1
         
-    else:
-      for j in range(len(row_st_ind)):
+    else: ##only other senario is there there was a merge
+      for j in range(len(row_st_ind)): ##organize the indecies to make the list in order
         row_merge.append([row_st_ind[j], row_en_ind[j]
     
-    row_st_count = 0
+    row_st_count = 0 ##clear all the necessary variables
     row_en_count = 0
     row_st_ind = []
     row_en_ind = []
 
-  match[rownum]=row_match
+  match[rownum]=row_match ##at the end of each row, append all lists to thier espective dictionaries
   short[rownum]=row_short
   merge[rownum]=row_merge
                           
-for clump in maybe_lonely:
+#for clump in maybe_lonely: ##later must set a limits on the minimumm number of times something has to appear in maybe lonely for it to actually be considered lonely
    
   
                          
   
-##err = 10 ##set allowable amount of error
-# n = 0
-# for j in range(len(df2_clumps)):
-#   split_inst = []
-#   merge_inst =[]
-#   if df1_en[n] == df2_en[j] and df1_st[n] == df2_st[j]: ##conditions for identical matches
-#     match[str(n)] = j
-#     n += 1
-#   elif ((df2_en[j] - err) <= df1_en[n] <= (df2_en[j] + err)) or ((df2_st[j] - err) <= df1_st[n] <= (df2_st[j] + err)): ##conditions for slight errors
-#     slight_off[str(n)] = j
-#     n += 1
-#   ##elif ((df1_st[n] == df2_st[j]) and (df1_en[n] != df2_en[j]) or (df1_en[n] == df2_en[j] and df1_st[n] != df2_st[j]) or ## commented out so still able to be seen
-#   elif df2_st[j] >= df1_st[n] and df2_en[j] <= df1_en[n]: ##conditions for a split 
-#     split_inst.append(j)
-#     if df1_en[n] == df2_en[j]:
-#        split[str(n)]= split_inst
-#        split_inst =[]
-#        n+=1
-#     else:
-#        continue
-#   elif df1_st[n] >= df2_st[i] and df1_en[n] <= df2_en[i]: ##conditions for a merge 
-#     merge_inst.append(j)
-#     if df1_en[n] == df2_en[j]:
-#        merge[str(n)]= merge_inst
-#        merge_inst =[]
-#        n+=1
-#     else:
-#        continue
-#   elif :  
