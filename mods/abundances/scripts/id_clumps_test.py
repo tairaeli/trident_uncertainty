04275cd643e9a9f1cbae22fa2594abf2c8d4fbce
amrollins <102754385+amrollins@users.noreply.github.com>
2022-06-09 10:43:35 -0400

    made the 2s when things are clumping together work

--- mods/abundances/scripts/id_clumps_test.py
+++ mods/abundances/scripts/id_clumps_test.py
@@ -6,36 +6,57 @@ path = "/mnt/scratch/f0104093/condensed_pipeline_tests/data/"
 datanum = 26 ##number of rows on the abundance table, modify as needed
 ndigits= len(str(datanum))
 raynum = 4 ##number of rays used, modify as needed
-problems = []
+
 
 for r in range(raynum):
-	rowlist = []
-	for i in range(datanum):
-		m = i+1
-		n_len = len(str(m))
-		n_zeros = ndigits - n_len
-		k = "0" * n_zeros + str(m)
-		row_data = pd.read_csv(path+f"data_AbundanceRow{k}_C_IV.txt", delim_whitespace=True) ##read in data files
-		row_work = row_data[row_data["lightray_index"]==r] ##filter to only ray1
-		df = row_work[["interval_start","interval_end"]].reset_index().drop(columns="index") ##filter to only indexes
-		rowlist.append(df)
+    rowlist = []
+    for i in range(datanum):
+        m = i+1
+        n_len = len(str(m))
+        n_zeros = ndigits - n_len
+        k = "0" * n_zeros + str(m)
+        row_data = pd.read_csv(path+f"data_AbundanceRow{k}_C_IV.txt", delim_whitespace=True) ##read in data files
+        row_work = row_data[row_data["lightray_index"]==r] ##filter to only ray1
+        df = row_work.reset_index().drop(columns="index") ##make indexing work
+        rowlist.append(df)
 
-	mx= -np.inf  ##find how long each array should be
-	for ds in rowlist: #find the cell index of the furthest clump
-		row_mx = max(ds["interval_end"])
-		if row_mx>mx:
-			mx=row_mx
-	super_clumps = np.zeros(int(mx))
-	clmaps = []
-	for ds in rowlist: ##make masks for each row and form super_clumps
-		ds_clump_loc = np.zeros(int(mx))
-		for i in range(1, ds.shape[0]):
-			ds_clump_loc[int(ds["interval_start"][i]):int(ds["interval_end"][i])] = 1
-			super_clumps[int(ds["interval_start"][i]):int(ds["interval_end"][i])] = 1
-		clmaps.append(ds_clump_loc)
+    mx= -np.inf  ##find how long each array should be
+    for ds in rowlist: #find the cell index of the furthest clump
+        row_mx = max(ds["interval_end"])
+        if row_mx>mx:
+            mx=row_mx
+    super_clumps = np.zeros(int(mx))
+    clmaps = []
+    for ds in rowlist: ##make masks for each row and form super_clumps
+        ds_clump_loc = np.zeros(int(mx))
+        for j in range(ds.shape[0]):
+            ds_clump_loc[int(ds["interval_start"][j]):int(ds["interval_end"][j])] = 1
+            
+            if j-1 == -1:
+                super_clumps[int(ds["interval_start"][j]):int(ds["interval_end"][j])] = 1
+            
+            elif int(ds["interval_end"][j-1]) == int(ds["interval_start"][j]):
+                ds_clump_loc[int(ds["interval_start"][j])] = 2
+                
+                if (super_clumps[int(ds["interval_end"][j-1])] == 0) or (super_clumps[int(ds["interval_end"][j-1])] == 2):
+                    super_clumps[int(ds["interval_start"][j]):int(ds["interval_end"][j])] = 1
+                    super_clumps[int(ds["interval_start"][j])] = 2
+                    print(f'2 is done on: {int(ds["interval_start"][j])}')
+                
+                else:
+                    super_clumps[int(ds["interval_start"][j]):int(ds["interval_end"][j])] = 1
+                
+                
+            else:
+                print(f'2 is erased on: {int(ds["interval_start"][j])}, {int(ds["interval_end"][j])}')
+                super_clumps[int(ds["interval_start"][j]):int(ds["interval_end"][j])] = 1 ##FIXME, overwrites 2s on last iteration
+
+	clmaps.append(ds_clump_loc)
+	
 	super_clumps=np.append(0, super_clumps)  ##make indexing work  
 	super_clumps=np.append(super_clumps, 0)
-	np.save(f'super_clumps_array_ray{r}', super_clumps)
+	sup_st_list = []
+	sup_en_list = []
                     
 	match = {} ##create dictionaries to store indexes of clumps in the row that correspond to one another, keys will be row numbers and values will be indecies except for the lonlies
 	short = {}
@@ -60,36 +81,45 @@ for r in range(raynum):
 		row_merge = []
     
 		for i in range(1,len(row)):
+			
 			if super_clumps[i-1]<super_clumps[i]: ##start of a super clump
-				sup_st = i-1 ##keep track of start location of a super clump
-			if row[i-1]<row[i]:  ##start of a clump in the row
-				row_st_cnt += 1
-				row_st_ind.append(i-1)
-        
-			elif row[i-1]>row[i]: ##end of a clump in row
-				row_en_cnt += 1
-				row_en_ind.append(i-1)
-            
-			if super_clumps[i-1]>super_clumps[i]: ##end of a super clump
-				sup_en = i-1 ##keep track of the location of the end of a super clump
-          
-				if (row_st_cnt == 1) and (row_en_cnt == 1): ##check for if there is only one row clump in the super clump
-    
-					if (row_st_ind[0] == sup_st) & (row_en_ind[0] == sup_en): ##if the starts and ends match, the clumps are identical
-						row_match.append([row_st_ind[0],row_en_ind[0]]) ##thus, start and end indecies appended to a list of them
+				sup_st = i-1##keep track of start location of a super clump
+				if sup_st in sup_st_list:
+					continue
+				else:
+					sup_st_list.append(sup_st)
+					
+				if row[i-1]<row[i]:  ##start of a clump in the row
+					row_st_cnt += 1
+					row_st_ind.append(i-1)
+
+				elif row[i-1]>row[i]: ##end of a clump in row
+					row_en_cnt += 1
+					row_en_ind.append(i-1)
+
+				if super_clumps[i-1]>super_clumps[i]: ##end of a super clump
+					sup_en = i-1 ##keep track of the location of the end of a super clump
+					if sup_en in sup_en_list:
+						continue
 					else:
-						row_short.append([row_st_ind[0],row_en_ind[0]]) ##if not, then the row clump must be shorter and the start and end indecies are appended to the appropraite list
-            	
-				elif (row_st_cnt == 0) & (row_en_cnt == 0): ##check if there is nothing in the row that matches the super clump
-                
-					if str([sup_st,sup_en]) in maybe_lonely.keys(): ##check if we have already seen this superclump, if not make the entry in the dictionary
-						maybe_lonely[str([sup_st,sup_en])] += 1
-					else:
-						maybe_lonely[str([sup_st,sup_en])] = 1
-				else: ##only other senario is there there was a merge
-					for j in range(len(row_st_ind)): ##organize the indecies to make the list in order
-						row_merge.append([row_st_ind[j],row_en_ind[j]]) 
-                                 
+						sup_en_list.append(sup_en)
+					if (row_st_cnt == 1) and (row_en_cnt == 1): ##check for if there is only one row clump in the super clump
+
+						if (row_st_ind[0] == sup_st) & (row_en_ind[0] == sup_en): ##if the starts and ends match, the clumps are identical
+							row_match.append([row_st_ind[0],row_en_ind[0]]) ##thus, start and end indecies appended to a list of them
+						else:
+							row_short.append([row_st_ind[0],row_en_ind[0]]) ##if not, then the row clump must be shorter and the start and end indecies are appended to the appropraite list
+
+					elif (row_st_cnt == 0) and (row_en_cnt == 0): ##check if there is nothing in the row that matches the super clump
+
+						if str([sup_st,sup_en]) in maybe_lonely.keys(): ##check if we have already seen this superclump, if not make the entry in the dictionary
+							maybe_lonely[str([sup_st,sup_en])] += 1
+						else:
+							maybe_lonely[str([sup_st,sup_en])] = 1
+					else: ##only other senario is there there was a merge
+						for j in range(len(row_st_ind)): ##organize the indecies to make the list in order
+							row_merge.append([row_st_ind[j],row_en_ind[j]]) 
+
 				row_st_cnt = 0
 				row_st_ind = []
 				row_en_cnt = 0
