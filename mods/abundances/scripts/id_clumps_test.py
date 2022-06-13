9b94b6e1b03e0a04de7bc438b4e3c7876d9376a1
amrollins <102754385+amrollins@users.noreply.github.com>
2022-06-13 16:40:24 -0400

    Works for all of OIV now

--- mods/abundances/scripts/id_clumps_test.py
+++ mods/abundances/scripts/id_clumps_test.py
@@ -7,7 +7,7 @@ datanum = 26 ##number of rows on the abundance table, modify as needed
 ndigits= len(str(datanum))
 raynum = 4 ##number of rays used, modify as needed
 
-for r in range(1):
+for r in range(raynum):
     rowlist = []
     for i in range(datanum):
         m = i+1
@@ -38,10 +38,10 @@ for r in range(1):
                 super_clumps[int(ds["interval_start"][j]):int(ds["interval_end"][j])] = 1
 
             elif int(ds["interval_end"][j-1]) == int(ds["interval_start"][j]):
-                
+
+                #print(f"{float(ds['delta_v'][j-1]) - 1.5 * (float(ds['vel_dispersion'][j-1]))}, {float(ds['delta_v'][j])}, {(1.5 * (float(ds['delta_v'][j-1])) + (float(ds['vel_dispersion'][j-1])))}")
                 if not (float(ds["delta_v"][j-1]) - 1.5 * (float(ds["vel_dispersion"][j-1]))) <= float(ds["delta_v"][j]) <= 1.5 * (float(ds["delta_v"][j-1]) + (float(ds["vel_dispersion"][j-1]))):
                     ds_clump_loc[int(ds["interval_start"][j])] = 2
-                    
                     if int(ds["interval_end"][j-1]) not in problems:
                         problems.append(int(ds["interval_end"][j-1]))
                     
@@ -61,18 +61,16 @@ for r in range(1):
         
         for index in problems:
             for row in clmaps:
-                if (row[index-1] == 0 and row[index+1] != 0) or (row[index+1] == 0 and row[index-1] != 0):
+                if ((row[index-1] == 0 and row[index+1] != 0) or (row[index+1] == 0 and row[index-1] != 0)) and (row[index] != 0):
                     super_clumps[index] = 2
+                    
                 
         
     
     super_clumps=np.append(0, super_clumps)  ##make indexing work  
     super_clumps=np.append(super_clumps, 0)
-    super_clumps=np.append(super_clumps, 0)
-    super_clumps[1784]=1
-    super_clumps[32] =1
-    super_clumps[1681] =1
-    np.save(f'super_clumps_array_O_IV_ray{r}', super_clumps)
+#     super_clumps[1784]=1
+    np.save(f'super_clumps_array_O_VI_ray{r}', super_clumps)
     match = {} ##create dictionaries to store indexes of clumps in the row that correspond to one another, keys will be row numbers and values will be indecies except for the lonlies
     short = {}
     merge = {}
@@ -82,7 +80,6 @@ for r in range(1):
     
     for row in clmaps:  ##start by iterating over a whole row
         row = np.append(0,row) # adding an extra element to prevent booleans from failing
-        row = np.append(row,0)
         row = np.append(row,0)
         rownum += 1 ##define which row we're working on
         ##make all the variables and lists necessary
@@ -95,7 +92,7 @@ for r in range(1):
         row_short = []
         row_merge = []
         sup_st_true =[]
-        problem = 0
+        weird_index = 0
 
         for i in range(len(row)):
             
@@ -103,13 +100,14 @@ for r in range(1):
                 sup_st = i-1 ##keep track of start location of a super clump
                 sup_st_true.append(sup_st)
                 
-            if row[i-1]<row[i]:##start of a clump in the row
+            if row[i-1]<row[i] and len(row_st_ind) == 0:##start of a clump in the row
                 if super_clumps[i-1] == 2:
                     print("oops")
-                else:
-                   
+                elif super_clumps[i] != 2 or len(row_st_ind)!=0:
                     row_st_ind.append(i-1)
                     row_st_cnt += 1
+                else:
+                    weird_index = i-1
 
             elif row[i-1]>row[i]: ##end of a clump in row
                 row_en_cnt += 1
@@ -122,27 +120,21 @@ for r in range(1):
                 if super_clumps[i-1]==2:
                     sup_en = i-2
                     sup_st = sup_st_true[0]
-                if problem != 0 and (len(sup_st_true) >= 2):
-
-                    sup_st = sup_st_true[1]
-#                     
                     
                 if super_clumps[i-1] == 0 or super_clumps[i-1] == 1:
                     sup_en = i-1 ##keep track of the location of the end of a super clump
                 
                
+               
                 if (row_st_cnt == 1) or (row_en_cnt == 1): ##check for if there is only one row clump in the super clump
-                    
-                   
-                    if sup_en == row_st_ind[0]:
+                    if weird_index != 0:
+                        row_st_ind.append(weird_index)
                         
-#                         problem += 1
-                         continue
-                   
+                    if len(row_en_ind) == 0:
+                        break
                     if (row_st_ind[0] == sup_st) and (row_en_ind[0] == sup_en): ##if the starts and ends match, the clumps are identical
                         row_match.append([row_st_ind[0],row_en_ind[0]]) ##thus, start and end indecies appended to a list of them
                     else:
-                        print(rownum, sup_st, sup_en, row_st_ind, row_en_ind)
                         row_short.append([row_st_ind[0],row_en_ind[0]]) ##if not, then the row clump must be shorter and the start and end indecies are appended to the appropraite list
                         
                 elif (row_st_cnt == 0) and (row_en_cnt == 0): ##check if there is nothing in the row that matches the super clump
@@ -173,24 +165,24 @@ for r in range(1):
                     if row[i-1]>row[i]:
                         row_st_ind.append(i-2)
                         row_st_cnt += 1
+                        
+                
                           
-#for clump in maybe_lonely: ##later must set a limits on the minimumm number of times something has to appear in maybe lonely for it to actually be considered lonely
-	print(match)
-	print(merge)
-	print(short)
-	pickling_match = open(f"MatchRay{r}.pickle","wb") ##saves the dictonaries so that they can be accesssed later
-	pickle.dump(match, pickling_match, protocol=3)	
-	pickling_match.close()
+# #for clump in maybe_lonely: ##later must set a limits on the minimumm number of times something has to appear in maybe lonely for it to actually be considered lonely
+
+    pickling_match = open(f"MatchOVIRay{r}.pickle","wb") ##saves the dictonaries so that they can be accesssed later
+    pickle.dump(match, pickling_match, protocol=3)	
+    pickling_match.close()
                           
-	pickling_merge = open(f"MergeRay{r}.pickle","wb")
-	pickle.dump(merge, pickling_merge, protocol=3)
-	pickling_merge.close() 
-                          
-	pickling_short = open(f"ShortRay{r}.pickle","wb")
-	pickle.dump(short, pickling_short, protocol=3)
-	pickling_short.close()
-	
-	pickling_maybe_lonely = open(f"MaybeLonelyRay{r}.pickle","wb")
-	pickle.dump(maybe_lonely, pickling_maybe_lonely, protocol=3)
-	pickling_maybe_lonely.close()
+    pickling_merge = open(f"MergeOVIRay{r}.pickle","wb")
+    pickle.dump(merge, pickling_merge, protocol=3)
+    pickling_merge.close() 
+                      
+    pickling_short = open(f"ShortOVIRay{r}.pickle","wb")
+    pickle.dump(short, pickling_short, protocol=3)
+    pickling_short.close()
+    
+    pickling_maybe_lonely = open(f"MaybeLonelyOVIRay{r}.pickle","wb")
+    pickle.dump(maybe_lonely, pickling_maybe_lonely, protocol=3)
+    pickling_maybe_lonely.close()
 	
