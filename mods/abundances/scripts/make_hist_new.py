f6358ee57f1d1ed9626afc59b88b10d924fdc1e5
amrollins <102754385+amrollins@users.noreply.github.com>
2022-06-13 17:37:37 -0400

    changed merge to split

--- mods/abundances/scripts/make_hist_new.py
+++ mods/abundances/scripts/make_hist_new.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-pickle_match_off = open("MatchRay1.pickle", 'rb')
+pickle_match_off = open("MatchCIIRay0.pickle", 'rb')
 match = pickle.load(pickle_match_off)
 
-pickle_merge_off = open("MergeRay1.pickle", 'rb')
-merge = pickle.load(pickle_merge_off)
+pickle_split_off = open("SplitCIIRay0.pickle", 'rb')
+split = pickle.load(pickle_split_off)
 
-pickle_short_off = open("ShortRay1.pickle", 'rb')
+pickle_short_off = open("ShortCIIRay0.pickle", 'rb')
 short = pickle.load(pickle_short_off)
 
-super_clumps = np.load('super_clumps_array_ray1.npy')
+super_clumps = np.load('super_clumps_array_C_II_ray0.npy')
 
 var_rows = []
 path = "/mnt/scratch/f0104093/condensed_pipeline_tests/data/"
@@ -23,8 +23,8 @@ for i in range(datanum):
   n_len = len(str(m))
   n_zeros = ndigits - n_len
   p = "0" * n_zeros + str(m)
-  row_data = pd.read_csv(path+f"data_AbundanceRow{p}_C_IV.txt", delim_whitespace=True) ##read in data files
-  row_work = row_data[row_data["lightray_index"]==1] ##filter to only ray1
+  row_data = pd.read_csv(path+f"data_AbundanceRow{p}_C_II.txt", delim_whitespace=True) ##read in data files
+  row_work = row_data[row_data["lightray_index"]==0] ##filter to only one ray
   df = row_work.reset_index().drop(columns="index") ##make indexing work
   var_rows.append(df)
 
@@ -46,7 +46,7 @@ for i in range(1, len(super_clumps)):
 	
 for k in range(len(sup_st)): ##depending on which category each clump belongs to in super_clumps, append its column density to a list
     col_density_match = []
-    col_density_merge = []
+    col_density_split = []
     col_density_short = []
 
     for row, index in match.items():
@@ -56,7 +56,7 @@ for k in range(len(sup_st)): ##depending on which category each clump belongs to
                 indexq = np.where((index[j][0]) == (var_rows[row-1]["interval_start"]))
                 col_density_match.append(ds["col_dens"][int(indexq[0])])
 
-    for rowm, indexm in merge.items(): ##merge is a bit weird so we have to average the densities maybe should sum though?
+    for rowm, indexm in split.items(): ##split is a bit weird so we have to average the densities maybe should sum though?
         temp_col_dens =[]
         for j in range(len(indexm)):
             if (indexm[j][0]>=sup_st[k]) and (indexm[j][1]<=sup_en[k]):
@@ -65,7 +65,7 @@ for k in range(len(sup_st)): ##depending on which category each clump belongs to
                 temp_col_dens.append(10 ** ds["col_dens"][int(indexq[0])])
         if len(temp_col_dens) != 0:
             log_sum_dens = np.log10(sum(temp_col_dens))
-            col_density_merge.append(log_sum_dens)
+            col_density_split.append(log_sum_dens)
 
     for rows, indexs in short.items():
         for j in range(len(indexs)):
@@ -76,10 +76,10 @@ for k in range(len(sup_st)): ##depending on which category each clump belongs to
 
     
    ##plot the results##
-    plt.hist((col_density_match, col_density_merge, col_density_short), histtype='barstacked', label=['col_density_match', 'col_density_merge', 'col_density_short', 'col_density_false_merge'])
+    plt.hist((col_density_match, col_density_split, col_density_short), histtype='barstacked', label=['col_density_match', 'col_density_split', 'col_density_short', 'col_density_false_split'])
     plt.legend()
-    plt.title(f"C_IV -- LightRay Index 1 -- Super Clump {k}")
-    plt.savefig(f"/mnt/scratch/f0104093/condensed_pipeline_tests/visuals/super_clump_hist/Hist_CIV_RayIndex1_SuperClump{k}_.png")
+    plt.title(f"C_II -- LightRay Index 0 -- Super Clump {k}")
+    plt.savefig(f"/mnt/scratch/f0104093/condensed_pipeline_tests/visuals/super_clump_hist/Hist_CII_RayIndex0_SuperClump{k}_.png")
     plt.close()
     print("Plotted!")
 
