fddde6630cd12db553cca6d18b0d7acc03e50104
Elias Taira <tairaeli@msu.edu>
2022-06-14 14:51:05 -0400

    making code similar to Alexis'

--- mods/abundances/scripts/sal_the_snek.py
+++ mods/abundances/scripts/sal_the_snek.py
@@ -55,7 +55,7 @@ def generate_names(length, add=''):
 ds = yt.load(args.ds_file)
 center = ds.arr([23876.757358761424, 23842.452527236022, 22995.717805638298], 'kpc')
 gal_vel = ds.arr([-0.02410298432958413, -136.9259851493111, -147.88486721075668], 'km/s')
-other_fields=['density', 'temperature', 'metallicity']
+other_fields=['density', 'temperature', 'metallicity', 'radius']
 max_impact=15 #kpc
 units_dict = dict(density='g/cm**3', metallicity='Zsun')
 
@@ -68,12 +68,11 @@ np.random.seed(11)
 
 # CK: Check that rays already exist, and that the have the additional fields contained
 # in the third argument (empty for now; might become a user parameter)
-
 check = check_rays(path+"/rays", args.nrays, [])
 if not check:
     print("WARNING: rays not found. Generating new ones.")
-    salsa.generate_lrays(ds, center.to('code_length'), args.nrays, max_impact, ion_list=['H I'], fields=other_fields, out_dir=(path + "/rays"),field_parameters={'bulk_velocity', gal_vel})
-    
+    salsa.generate_lrays(ds, center.to('code_length'), args.nrays, max_impact, length=600, field_parameters={'bulk_velocity', gal_vel}, ion_list=['H I'], fields=other_fields, out_dir=(path + "/rays"))
+
 ray_list=[]
 for i in range(args.nrays):
     if len(str(i)) != len(str(args.nrays)):
@@ -98,7 +97,7 @@ if 'file_path' in dic_args:
 	for row_num in range(nrows):
 		for i in ion_list:
 			abundances = abun.iloc[row_num].to_dict()
-			abs_ext = salsa.AbsorberExtractor(ds, ray_file, ion_name = i, abundance_table = abundances, calc_missing=True)
+			abs_ext = salsa.AbsorberExtractor(ds, ray_file, ion_name = i, velocity_res =20, abundance_table = abundances, calc_missing=True)
 			df = salsa.get_absorbers(abs_ext, my_rays, method='spice', fields=other_fields, units_dict=units_dict)
 			df.to_csv(f'{args.path}/data/{saved[row_num]}_{i.replace(" ", "_")}.txt', sep = ' ')
 			print("Go look at your data!")
@@ -108,7 +107,6 @@ else:
 	saved = generate_names(nrows)
 	for i in ion_list:
 		abs_ext = salsa.AbsorberExtractor(ds, ray_file, ion_name = i, abundance_table = None, calc_missing=True)
-		print(type(abs_ext))
 		df = salsa.get_absorbers(abs_ext, my_rays, method='spice', fields=other_fields, units_dict=units_dict)
 		df.to_csv(f'{args.path}/data/data_SolAb_{i.replace(" ", "_")}.txt', sep = ' ')
 		print("Go look at your data!")
