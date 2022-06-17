5c9e1c4a2be8ccd33276b58c9b8561564503f3a7
amrollins <102754385+amrollins@users.noreply.github.com>
2022-06-17 09:49:07 -0400

    commented

--- mods/abundances/scripts/clump_stats.py
+++ mods/abundances/scripts/clump_stats.py
@@ -1,12 +1,12 @@
 import pickle
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy import stats
 
 raynum = 4 ##number of rays we have
-ion = "C_IV"
+ion = "C_IV" ##which ion to analyse
 
+##necessary lists
 ray_nums = []
 super_cl_nums = []
 med_col_dens = []
@@ -19,6 +19,7 @@ temperatures = []
 num_clumps = []
 rows_of_rep_clumps = []
 
+##necessary functions
 def weighted_av(values, weights):
     weighted_sum = []
     for value, weight in zip(values, weights):
@@ -32,7 +33,7 @@ def make_full_list(list_in, list_out):
         return list_out
 
 for r in range(raynum):
-    pickle_match_off = open(f"Match_{ion}_Ray{r}.pickle", 'rb')
+    pickle_match_off = open(f"Match_{ion}_Ray{r}.pickle", 'rb') ##get data from id_clumps
     match = pickle.load(pickle_match_off)
 
     pickle_split_off = open(f"Split_{ion}_Ray{r}.pickle", 'rb')
@@ -48,7 +49,7 @@ for r in range(raynum):
     datanum = 26 ##number of rows on the abundance table, modify as needed
     ndigits= len(str(datanum))
     for i in range(datanum):
-        m = i+1
+        m = i+1 ##correct filenames
         n_len = len(str(m))
         n_zeros = ndigits - n_len
         p = "0" * n_zeros + str(m)
@@ -72,31 +73,26 @@ for r in range(raynum):
             else:
                 sup_en.append(n)
                 
-    print(sup_st)
-    print(sup_en)
 
     for k in range(len(sup_st)): ##depending on which category each clump belongs to in super_clumps, append its column density to a list
         col_density_match = []
         col_density_split = []
         col_density_short = []
-        match_done = []
+        match_done = [] ##keep track of where things have and haven't been accounted for
         short_done = []
         split_done = []
 
         for row, index in match.items():
         
-            for j in range(len(index)):
+            for j in range(len(index)): ##iterate over the list of indecies in the dictionary
 
-                if (index[j][0]>=sup_st[k]) and (index[j][1]<=sup_en[k]):
-                    
+                if (index[j][0]>=sup_st[k]) and (index[j][1]<=sup_en[k]): ##how to differentaite between super_clumps
                     ds = var_rows[row-1]
-                    indexq = np.where((index[j][0]) == (ds["interval_start"]))
-                    
-#                     if len(list(indexq[0]))>0:
-                    col_density_match.append(ds["col_dens"][int(indexq[0])])
+                    indexq = np.where((index[j][0]) == (ds["interval_start"])) ##find the correct row in the dataset
+                    col_density_match.append(ds["col_dens"][int(indexq[0])]) 
                 
-                    if sup_st[k] not in match_done:
-                        print(row, indexs[j][0], sup_st[k])
+                    if sup_st[k] not in match_done: ##for each super_clump, get each of these quantites once
+                        print(row, index[j][0], sup_st[k])
                         #distances.append(ds["radius"][int(indexq[0])])
                         central_v.append(ds["delta_v"][int(indexq[0])])
                         vel_dispersions.append(ds["vel_dispersion"][int(indexq[0])])
@@ -106,7 +102,7 @@ for r in range(raynum):
                         match_done.append(sup_st[k])
                         
 
-        for rows, indexs in short.items():
+        for rows, indexs in short.items(): ##same procedure as match
             
             for j in range(len(indexs)):
                 
@@ -115,8 +111,8 @@ for r in range(raynum):
                     indexq = np.where((indexs[j][0]) == (var_rows[rows-1]["interval_start"]))
                     col_density_short.append(ds["col_dens"][int(indexq[0])])
                     
-                    if len(match_done) == 0 and (sup_st[k] not in short_done):
-                        print(row, index[j][0], sup_st[k])
+                    if len(match_done) == 0 and (sup_st[k] not in short_done): ##only do this if there isn't a match
+                        print(rows, indexs[j][0], sup_st[k])
                         #distances.append(ds["radius"][int(indexq[0])])
                         central_v.append(ds["delta_v"][int(indexq[0])])
                         vel_dispersions.append(ds["vel_dispersion"][int(indexq[0])])
@@ -125,7 +121,8 @@ for r in range(raynum):
                         rows_of_rep_clumps.append(row)
                         short_done.append(sup_st[k])
 
-        for rowm, indexm in split.items(): ##split is a bit weird so we have to average the densities maybe should sum though?
+        for rowm, indexm in split.items(): ##split is a bit weird
+        ##if necessary to use a split as a "representative, these lists are how we make the weighted average
             temp_col_dens =[]
             temp_delta_v = []
             temp_vel_dis = []
@@ -141,7 +138,7 @@ for r in range(raynum):
                     indexq = np.where((indexm[j][0]) == (var_rows[rowm-1]["interval_start"]))
                     
                     if len(list(indexq[0])) > 0:
-                        temp_col_dens.append(10 ** ds["col_dens"][int(indexq[0])])
+                        temp_col_dens.append(10 ** ds["col_dens"][int(indexq[0])]) ##column density must be summed to give a coreect value
                         col_dens_for_weights.append(ds["col_dens"][int(indexq[0])])
                         temp_delta_v.append(ds["delta_v"][int(indexq[0])])
                         temp_vel_dis.append(ds["vel_dispersion"][int(indexq[0])])
@@ -150,7 +147,7 @@ for r in range(raynum):
                         #temp_rad.append(ds["radius"][int(indexq[0])])
 						
                         
-                        if len(match_done) == 0 and len(short_done) == 0 and sup_st[k] not in split_done:
+                        if len(match_done) == 0 and len(short_done) == 0 and sup_st[k] not in split_done: ##only do this if no match or shorter
                             #distances.append(weighted_av(temp_rad, col_dens_for_weights))
                             central_v.append(weighted_av(temp_delta_v, col_dens_for_weights))
                             vel_dispersions.append(weighted_av(temp_vel_dis, col_dens_for_weights))
@@ -159,10 +156,9 @@ for r in range(raynum):
                             rows_of_rep_clumps.append(row)
                             split_done.append(sup_st[k])
                             
-
-                if len(temp_col_dens) != 0:
-                    log_sum_dens = np.log10(sum(temp_col_dens))
-                    col_density_split.append(log_sum_dens)
+                    if len(temp_col_dens) != 0:
+                        log_sum_dens = np.log10(sum(temp_col_dens))
+                        col_density_split.append(log_sum_dens)
 
       ##make lists to put into dictionaries
         ray_nums.append(r)
@@ -177,6 +173,7 @@ for r in range(raynum):
       
         num_clumps.append(len(full_col_density))
         
+##make and fill the dictionary that becomes our dataframe
 clump_stats = {}
 clump_stats["ray_num"] = ray_nums
 clump_stats["super_clump_number"] = super_cl_nums 
@@ -189,8 +186,6 @@ clump_stats["density"] = densities
 clump_stats["temperature"] = temperatures
 clump_stats["num_of_clumps"] = num_clumps
 clump_stats["rep_clumps_row"] = rows_of_rep_clumps
-
-print(len(ray_nums), len(super_cl_nums), len(med_col_dens), len(mad_for_med), len(central_v), len(vel_dispersions))
 
 df = pd.DataFrame.from_dict(clump_stats)
 df.to_csv(f"halo_rshift_{ion}_abun_model-family_all-clumps.csv" ,sep = ' ')
