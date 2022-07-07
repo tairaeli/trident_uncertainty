fed49c8433c43c40194ff392d621087b0460d7bf
amrollins <102754385+amrollins@users.noreply.github.com>
2022-07-07 17:18:23 -0400

    made preliminary graph

--- mods/abundances/scripts/make_intersting_plots.py
+++ mods/abundances/scripts/make_intersting_plots.py
@@ -1,17 +1,35 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-halo_lis = [] ##list of halos to analyze
-rs_lis = [] ##list of rs to analyze, true
-ion_lis = [] ##list of ions to analyze
+halo_marker_dict = {'2392' : 'O', '4123' : 'x', '5016' : 'v', '5036' : '*', '8508' : 'D'} ##list of halos to analyze
+halo_names_dict = {'2392'  :  'Hurricane' ,'2878'  :  'Cyclone' , '4123'  :  'Blizzard' , '5016'  :  'Squall' ,'5036'  :  'Maelstrom' , '8508'  :  'Tempest'}
 
-for halo in halo_lis:
-  ##get names??
-  for rs in rs_lis:
-    for ion in ion_lis:
+def get_halo_names(num):
+    if str(num) in halo_names_dict.keys():
+        halo_name = halo_names_dict[str(num)]
+    return halo_name
+
+rs_lis = [2.0, 2.5] ##list of rs to analyze, true
+ion_dict = {'C_II':'C4', 'C_IV':'C2', 'O_VI': 'C1'} ##dictionary of ions to colors
+
+for rs in rs_lis:
+  for halo in halo_marker_dict.keys():
+    name = get_halo_names(halo)
+    for ion in ion_dict.keys():
       ds = pd.read_csv(f'/mnt/scratch/f0104093/cgm_abundance_variance/halo{halo}/redshift{rs}/stats/{halo}_z{rs}_{ion}_abun_all-model-families_all-clumps.csv)
       med_col_dens = ds["median_col_density"]
       col_dens_spread = ds["mad_for_col_density"]
+      plt.scatter(med_col_dens, col_dens_spread, c = ion_dict[ion], marker = halo_marker_dict[halo], label = f"{ion} in {name}")
+  plt.title(f"MAD of Column Density vs Column Density, Redshift {rs}")
+  plt.legend()
+  plt.xlabel("Median log(Column Density)")
+  plt.ylabel("MAD of log(Column Density)")
+  plt.savefig(f'/mnt/scratch/f0104093/cgm_abundance_variance/graphs/mad_vs_med_z{rs}.png')
+                       
+                       
+                       
+                       
       density = ds["density"]
       temperature = ds["temperature"]
       distance = ds["distance_from_galaxy"]
+      
