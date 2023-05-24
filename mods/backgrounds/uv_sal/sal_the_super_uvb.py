e12569a4f8bbbe6507af2b90e5afe5284430ad46
Elias Taira <tairaeli@msu.edu>
2023-05-24 16:30:10 -0400

    adding comments

--- mods/backgrounds/uv_sal/sal_the_super_uvb.py
+++ mods/backgrounds/uv_sal/sal_the_super_uvb.py
@@ -12,7 +12,7 @@ from scipy import stats
 import trident
 import configparser
 
-from uvb_abun_pairwize_compare import pairwise_compare
+from uvb_abun_pairwise_compare import pairwise_compare
 
 comm = MPI.COMM_WORLD
 
@@ -37,24 +37,24 @@ def get_halo_names(num):
 
 def generate_names(length, add=''):
         
-        	"""
-        	Returns a list of generic names for multiplots anda list of generic names for raw data. These lists are typically passed to run_sal()
-        	
-        	:length: length of lists to be generated. Do not account for indexing starting at zero.
-        	
-        	:add: Additional relevant information for keeping track of multiplots and data later on. Default add=''
-        	"""
-        	ndigits = len(str(length))
-        	saved_filename_list = []
-        	
-        	for i in range(length): ##made this so that it would sort correctly for making plots
-        		m= i+1
-        		n_len = len(str(m))
-        		n_zeros = ndigits - n_len
-        		k = "0" * n_zeros + str(m)
-        		saved_filename_list.append(f'data_AbundanceRow{k}{add}')
-        		
-        	return saved_filename_list
+	"""
+	Returns a list of generic names for multiplots anda list of generic names for raw data. These lists are typically passed to run_sal()
+	
+	:length: length of lists to be generated. Do not account for indexing starting at zero.
+	
+	:add: Additional relevant information for keeping track of multiplots and data later on. Default add=''
+	"""
+	ndigits = len(str(length))
+	saved_filename_list = []
+	
+	for i in range(length): ##made this so that it would sort correctly for making plots
+		m= i+1
+		n_len = len(str(m))
+		n_zeros = ndigits - n_len
+		k = "0" * n_zeros + str(m)
+		saved_filename_list.append(f'data_AbundanceRow{k}{add}')
+		
+	return saved_filename_list
 
 # defining analysis parameters
 # Note: these dictionaries are temporary and should most likely be included in the arguments at some point
@@ -85,6 +85,18 @@ true_rs = get_true_rs(int(rs))
 # outputs a dictionary of galactic center locations/velocities for all redshifts in each halo pattern
 # NOTE: this function is temporary and has some hard-coded variables that will need to be changed
 def foggie_defunker(foggie_dir):
+    """
+    Extracts data from FOGGIE simulation runs
+    
+    args:
+    
+        foggie_dir (str) - contains the path to FOGGIE data directory
+    
+    returns:
+    
+        center_dat (Dict) - contains info on the position and velocity of the
+                            given halo for a given redshift
+    """
     # initializing dictionary to store all of the galactic center data
     center_dat = {}
 
@@ -140,8 +152,6 @@ uvb_list = sal_args["uvb_analysis"]["uvb_files"].split(" ")
 
 # define desired uvbs analyzed
 uvb_names = sal_args["uvb_analysis"]["uvb_names"].split(" ")
-
-
 
 # defining analysis parameters
 # Note: these dictionaries are temporary and should most likely be included in the arguments at some point
@@ -231,12 +241,13 @@ salsa_out_dict = {}
 for ion in alt_ion_list:
     
     salsa_out_dict[ion] = {}
-    
+
     for iuvb, uvb in enumerate(uvb_list):
+            
         # impliments the ionization table for each different UVB model
         trident.ion_balance.add_ion_fields(ds, ions = alt_ion_list, ionization_table = uvb)
-        saved = generate_names(nrows,uvb_names[iuvb])
         
+        saved = generate_names(nrows,uvb_names[iuvb])
     
         try:
             abundances = abun.iloc[row_num].to_dict()
@@ -262,188 +273,13 @@ for ion in alt_ion_list:
             
             salsa_out_dict[ion][uvb_names[iuvb]] = df
             
-            df.to_csv(f'{dat_path}/{saved[row_num]}_{i.replace(" ", "_")}_null.txt')
-            
+            df.to_csv(f'{dat_path}/{saved[row_num]}_{ion.replace(" ", "_")}_null.txt')
             
 pickling_match = open(f'{dat_path}/salsa_out_dict.pickle',"wb") ##saves the dictonaries so that they can be accesssed later
 pickle.dump(salsa_out_dict, pickling_match, protocol=3)	
 pickling_match.close()
 
-compare_dict = pairwise_compare(salsa_out_dict, ion_list, raynum)
+compare_dict, col_dens_1, col_dens_2 = pairwise_compare(salsa_out_dict, alt_ion_list, nrays)
 
 
-    for r in range(raynum):
-            
-            for i in range(datanum):
-                m = i+1
-                n_len = len(str(m))
-                n_zeros = ndigits - n_len
-                p = "0" * n_zeros + str(m)
-                row_data = pd.read_csv(dat_path+f"/data_AbundanceRow{p}_{ion}.txt", delim_whitespace=True) ##read in data files
-                row_work = row_data[row_data["lightray_index"]==r] ##filter to only one ray
-                df = row_work.reset_index().drop(columns="index") ##make indexing work
-                var_rows.append(df)
-    
-            sup_st = [] ##get indexes of superclumps
-            sup_en = []
-            for i in range(1, len(super_clumps)):
-                n= i-1
-                if super_clumps[n]<super_clumps[i]: ##start of a super clump
-                    if super_clumps[n]== 2:
-                        sup_st.append(n-1)
-                    else: 
-                        sup_st.append(n)
-                elif super_clumps[n]>super_clumps[i]: ##end of a super clump
-                    if super_clumps[n] ==2:
-                        sup_en.append(n-1)
-                    else:
-                        sup_en.append(n)
-            num_spl_sho = 0
-    
-            for k in range(len(sup_st)): ##depending on which category each clump belongs to in super_clumps
-                col_density_match = [] ##lists for col_density
-                col_density_split = []
-                col_density_short = []
-                match_done = [] ##keep track of which super clumps already have a representative
-                short_done = []
-                split_done = []
-    
-                for row, index in match.items(): ##first, see what matches the super clump
-    
-                    for j in range(len(index)):
-    
-                        if (index[j][0]>=sup_st[k]) and (index[j][1]<=sup_en[k]):
-    
-                            ds = var_rows[row-1]
-                            indexq = np.where((index[j][0]) == (ds["interval_start"]))
-                            if row != 1: ##exclude solar abundance
-                                col_density_match.append(ds["col_dens"][int(indexq[0])]) ##get column density
-                            elif row == 1: ##keep frack of what the solar abundance is 
-                                sol_ab_col_dens = ds["col_dens"][int(indexq[0])]
-                            if sup_st[k] not in match_done: ##if this is the first one done for a super clump, get all the other data and make this clump a "representative" of the super clump
-                                print('Match:', row, index[j][0], sup_st[k])
-                                distances.append(ds["radius"][int(indexq[0])])
-                                central_v.append(ds["delta_v"][int(indexq[0])])
-                                vel_dispersions.append(ds["vel_dispersion"][int(indexq[0])])
-                                densities.append(ds["density"][int(indexq[0])])
-                                temperatures.append(ds["temperature"][int(indexq[0])])
-                                rows_of_rep_clumps.append(row)
-                                cat_rep_clump.append('match')
-                                match_done.append(sup_st[k])
-    
-    
-                for rows, indexs in short.items(): ##exactly the same method as match, but do it second bc if there is a match, we want to use that as our representataive
-    
-                    for j in range(len(indexs)):
-    
-                        if (indexs[j][0]>=sup_st[k]) and (indexs[j][1]<=sup_en[k]):
-                            num_spl_sho += 1
-                            ds = var_rows[rows-1]
-                            indexq = np.where((indexs[j][0]) == (var_rows[rows-1]["interval_start"]))
-                            if rows != 1:
-                                col_density_short.append(ds["col_dens"][int(indexq[0])]) ##get column density
-                            elif rows == 1:
-                                sol_ab_col_dens = ds["col_dens"][int(indexq[0])]
-    
-                            if (len(match_done) == 0) and (sup_st[k] not in short_done):
-                                print('Short:', row, indexs[j][0], sup_st[k])
-                                distances.append(ds["radius"][int(indexq[0])])
-                                central_v.append(ds["delta_v"][int(indexq[0])])
-                                vel_dispersions.append(ds["vel_dispersion"][int(indexq[0])])
-                                densities.append(ds["density"][int(indexq[0])])
-                                temperatures.append(ds["temperature"][int(indexq[0])])
-                                rows_of_rep_clumps.append(row)
-                                cat_rep_clump.append('short')
-                                short_done.append(sup_st[k])
-    
-                for rowm, indexm in split.items(): ##split is a bit weird bc there are multiple in a single super clump
-                    temp_col_dens =[] ##create temporary lists 
-                    temp_delta_v = []
-                    temp_vel_dis = []
-                    temp_dens = []
-                    temp_temp = []
-                    temp_rad = []
-                    col_dens_for_weights = []
-    
-                    for j in range(len(indexm)):
-    
-                        if (indexm[j][0]>=sup_st[k]) and (indexm[j][1]<=sup_en[k]):
-                            ds = var_rows[rowm-1]
-                            indexq = np.where((indexm[j][0]) == (var_rows[rowm-1]["interval_start"]))
-                                
-                            if len(list(indexq[0])) > 0:
-                                temp_col_dens.append(10 ** ds["col_dens"][int(indexq[0])]) ##col_dens is on a log scale, so to add it all togeher must make it the exponent of 10
-                                col_dens_for_weights.append(ds["col_dens"][int(indexq[0])]) 
-                                temp_delta_v.append(ds["delta_v"][int(indexq[0])])## append the other dictionaries with the temporary values
-                                temp_vel_dis.append(ds["vel_dispersion"][int(indexq[0])])
-                                temp_dens.append(ds["density"][int(indexq[0])])
-                                temp_temp.append(ds["temperature"][int(indexq[0])])
-                                temp_rad.append(ds["radius"][int(indexq[0])])
-    
-                                if (len(match_done) == 0) and (len(short_done) == 0) and (sup_st[k] not in split_done):
-                                    print('Split:', row, indexm[j][0], sup_st[k])
-                                    distances.append(weighted_av(temp_rad, col_dens_for_weights)) ##use the col_dens as a weight for each value so we get a weight average if a split is chosen as a "represenatative"
-                                    central_v.append(weighted_av(temp_delta_v, col_dens_for_weights))
-                                    vel_dispersions.append(weighted_av(temp_vel_dis, col_dens_for_weights))
-                                    densities.append(weighted_av(temp_dens, col_dens_for_weights))
-                                    temperatures.append(weighted_av(temp_temp, col_dens_for_weights))
-                                    rows_of_rep_clumps.append(row)
-                                    split_done.append(sup_st[k])
-                                    cat_rep_clump.append('split')
-    
-    
-                    if len(temp_col_dens) != 0 and rowm != 1: ##finally, get one value for the col_dens of the whole thing
-                        num_spl_sho +=1
-                        log_sum_dens = np.log10(sum(temp_col_dens))
-                        col_density_split.append(log_sum_dens)
-                    elif len(temp_col_dens) != 0 and rowm == 1:
-                        num_spl_sho +=1
-                        sol_ab_col_dens = np.log10(sum(temp_col_dens))
-    
-              ##make lists to put into dictionaries
-                ray_nums.append(r)
-                super_cl_nums.append(k)
-    
-                full_col_density = []
-                make_full_list(col_density_match, full_col_density)
-                make_full_list(col_density_split, full_col_density)
-                make_full_list(col_density_short, full_col_density)
-                med_col_dens.append(np.median(full_col_density))
-                mad_for_med.append(stats.median_abs_deviation(full_col_density))
-                
-                num_clumps.append(len(full_col_density))
-                freq_split_short.append(num_spl_sho)
-                num_spl_sho = 0
-                
-                if sol_ab_col_dens != 0: ##handle the case where there is no col_density for the solar abundance
-                    diff_from_sol.append(np.log10((10 ** sol_ab_col_dens) -(10 ** np.median(full_col_density))))
-                else:
-                    diff_from_sol.append(np.NaN)
 
-        ##make and fill the dictionary that will be convered into a csv file
-        clump_stats = {}
-        clump_stats["ray_num"] = ray_nums
-        clump_stats["super_clump_number"] = super_cl_nums 
-        clump_stats["median_col_desnity"] = med_col_dens 
-        clump_stats["mad_for_col_desnity"] = mad_for_med
-        # clump_stats["diff_from_solar_abun"] = diff_from_sol
-        # clump_stats["distance_from_galaxy"] = distances 
-        # clump_stats["central_velocity"] = central_v 
-        # clump_stats["vel_dispersion"] = vel_dispersions 
-        # clump_stats["density"] = densities 
-        # clump_stats["temperature"] = temperatures
-        # clump_stats["num_split_or_short"] = freq_split_short
-        # clump_stats["num_of_clumps"] = num_clumps
-        # clump_stats["rep_clump_row"] = rows_of_rep_clumps
-        # clump_stats["category_rep_clump"] = cat_rep_clump
-    
-        print(len(ray_nums), len(super_cl_nums), len(med_col_dens), len(mad_for_med), len(central_v), len(vel_dispersions))
-        
-        df = pd.DataFrame.from_dict(clump_stats)
-        df.to_csv(f"{stat_path}/{halo}_z{true_rs}_{ion}_abun_all-model-families_all-clumps.csv" ,sep = ' ', na_rep = 'NaN') ##save the files to scratch
-
-        ##as we're done with each file, delete it so we don't get residual data we don't need
-        os.remove(f"{stat_path}/Match_{ion}_Ray{r}.pickle")
-        os.remove(f"{stat_path}/Short_{ion}_Ray{r}.pickle")
-        os.remove(f"{stat_path}/Split_{ion}_Ray{r}.pickle")
-        os.remove(f'{stat_path}/super_clumps_array_{ion}_ray{r}.npy')
