89977c0d5c111b171405b0080a15c8a06ca4edab
Elias Taira <tairaeli@msu.edu>
2024-07-19 13:01:52 -0400

    forgot one

--- mods/backgrounds/uv_sal/pipeline/uvb_abun_pairwise_compare.py
+++ mods/backgrounds/uv_sal/pipeline/uvb_abun_pairwise_compare.py
@@ -4,7 +4,12 @@ Makes comparison between the SALSA output from 2 different Ultraviolet Backgroun
 
 import numpy as np
 import pandas as pd
+import pickle
+import configparser
+import argparse
+import os
 
+from condense_clumps import condense_pairwise_data
 
 def find_max_length(uvb_list):
     """
@@ -32,7 +37,7 @@ def find_max_length(uvb_list):
 def is_shorter(start_1, start_2, end_1, end_2, clump_error):
     '''
     Function for determining whether or not a given clump in the second abundance
-    data set was shorter or longer than the first abundance data set.
+    data set is shorter than the first abundance data set.
 
     args:
 
@@ -52,11 +57,70 @@ def is_shorter(start_1, start_2, end_1, end_2, clump_error):
     '''
     shorter = False
     
-    if (start_1 < start_2 - clump_error) or (end_1 > end_2 + clump_error):
+    if (start_1 <= start_2 + clump_error) and (end_1 >= end_2 - clump_error):
         shorter = True    
             
     return shorter
+
+def is_longer(start_1, start_2, end_1, end_2, clump_error):
+    '''
+    Function for determining whether or not a given clump in the second abundance
+    data set is longer than the first abundance data set.
+
+    args:
+
+        start_1 (int) - start position of the current clump 1
+
+        start_2 (int) - start position of the current clump 2
+
+        end_1 (int) - end position of current clump 1
+
+        end_2 (int) - end position of current clump 2
+
+        clump_error (int) - margin of error by which we consider corresponding clumps to be at equivalent indices
     
+    returns:
+
+        longer (bool) - whether clump 2 is longer than clump 1
+    '''
+    longer = False
+    
+    if (start_1 >= start_2 - clump_error) and (end_1 <= end_2 + clump_error):
+        longer = True    
+            
+    return longer
+
+def is_overlap(start_1, start_2, end_1, end_2, clump_error):
+    '''
+    Function for determining whether or not a given clump in the second abundance
+    data set overlaps, but doesn't quite line up with the first abundace set.
+
+    args:
+
+        start_1 (int) - start position of the current clump 1
+
+        start_2 (int) - start position of the current clump 2
+
+        end_1 (int) - end position of current clump 1
+
+        end_2 (int) - end position of current clump 2
+
+        clump_error (int) - margin of error by which we consider corresponding clumps to be at equivalent indices
+    
+    returns:
+
+        overlap (bool) - whether clump 2 overlaps with clump 1
+    '''
+    overlap = False
+    
+    if (end_2 >= start_1 - clump_error) and (start_2 <= start_1 + clump_error):
+        overlap = True   
+
+    elif (end_1 >= start_2 - clump_error) and (start_1 <= start_2 + clump_error):
+        overlap = True    
+            
+    return overlap
+
 
 def check_split(end_big, small_ends, id_small, clump_error):
     '''
@@ -79,11 +143,10 @@ def check_split(end_big, small_ends, id_small, clump_error):
         clumps_within (int) - number of smaller clumps within the larger clump 
     '''
     clumps_within = 0
-
     # iterates through initial clump + number of clumps after this clump that 
     # are still within the bounds of clump 1
 
-    while((id_small + clumps_within < len(small_ends)) and (small_ends[id_small + clumps_within] <= end_big - clump_error)):
+    while(((id_small + clumps_within) < len(small_ends)) and (small_ends[id_small + clumps_within] <= end_big - clump_error)):
         clumps_within +=1
     
     return clumps_within
@@ -111,14 +174,14 @@ def actually_lonely(id_comp, id_ques, uvb_comp, uvb_ques, clump_error):
         False - if the two clumps do not match
     """
 
-    if uvb_comp["interval_start"][id_comp] - clump_error <= uvb_ques["interval_start"][id_ques] <= uvb_comp["interval_start"][id_comp] + clump_error or \
-       uvb_comp["interval_end"][id_comp] - clump_error <= uvb_ques["interval_end"][id_ques] <= uvb_comp["interval_end"][id_comp] + clump_error:
+    if (uvb_comp["interval_start"][id_comp] - clump_error) <= uvb_ques["interval_start"][id_ques] <= (uvb_comp["interval_start"][id_comp] + clump_error) or \
+       (uvb_comp["interval_end"][id_comp] - clump_error) <= uvb_ques["interval_end"][id_ques] <= (uvb_comp["interval_end"][id_comp] + clump_error):
         
         return False
     else:
         return True
 
-def pairwise_compare(salsa_out, ion_list, nrays):
+def pairwise_compare(salsa_out, nrays):
     '''
     Compares two different SALSA abudnance tables to one another created from two
     different Ultraviolet Backgrounds
@@ -151,14 +214,22 @@ def pairwise_compare(salsa_out, ion_list, nrays):
         compare_dict[ion] = {}
         
         for ray in range(nrays):
+            str_ray = None
             
+            if len(str(ray)) != len(str(nrays)):
+                n = len(str(nrays))
+                
+                str_ray = f'{ray:0{n}d}'
+            else:
+                str_ray = f'{ray}'
+
             uvb_list = []
             
             for uvb_name in ion_dat.keys():
                 
                 uvb_dat = ion_dat[uvb_name]
-                
-                ray_dat = uvb_dat[uvb_dat["lightray_index"]==str(ray)].reset_index()
+
+                ray_dat = uvb_dat[uvb_dat["lightray_index"]==str_ray].reset_index()
                 
                 uvb_list.append(ray_dat)
                 
@@ -170,6 +241,7 @@ def pairwise_compare(salsa_out, ion_list, nrays):
             match = {} ##create dictionaries to store indexes of clumps in the row that correspond to one another, keys will be row numbers and values will be indices except for the lonlies
             shorter = {}
             longer = {}
+            overlap = {}
             split = {}
             merge = {}
             lonely_1 = []
@@ -179,7 +251,7 @@ def pairwise_compare(salsa_out, ion_list, nrays):
             id2 = 0
             
             mx = find_max_length(uvb_list)
-            print("mx",mx)
+            # print("mx",mx)
             while ((id1 < mx) and (id2 < mx)):
 
                 if id1 > len(uvb1["interval_start"])-1:
@@ -190,21 +262,32 @@ def pairwise_compare(salsa_out, ion_list, nrays):
 
                     id2 = len(uvb2["interval_start"])-1 
 
-                print("id1",id1)
-                print("id2",id2)
+                # checks for instances where length of data is 0
+                if id1 < 0:
+                    print(f"Something weird with ray: {ray}, ion: {ion} in uvb 1")
+                    break
+                
+                if id2 < 0:
+                    print(f"Something weird with ray: {ray}, ion: {ion} in uvb 2")
+                    break
+
+                # print("id1",id1)
+                # print("id2",id2)
 
                 if len(lonely_1) != 0 and len(lonely_2) != 0:
                     # if a lonely clump was just added, check to see if it's actually lonely
-                    if lonely_1[-1] == id1-1 or lonely_2[-1] == id2-1:
+                    if lonely_1[-1] == id1-1:
+
+                        if not actually_lonely(id2, id1-1, uvb2, uvb1, clump_error):
+                            lonely_1.remove(id1-1)
+                            id1-=1
+                    
+                    if lonely_2[-1] == id2-1:
                         
                         if not actually_lonely(id1, id2-1, uvb1, uvb2, clump_error):
                             lonely_2.remove(id2-1)
                             id2-=1
-
-                        elif not actually_lonely(id2, id1-1, uvb2, uvb1, clump_error):
-                            lonely_1.remove(id1-1)
-                            id1-=1
-
+     
                 start_1 = uvb1["interval_start"][id1]
                 start_2 = uvb2["interval_start"][id2]
                 
@@ -218,68 +301,68 @@ def pairwise_compare(salsa_out, ion_list, nrays):
                     id1+=1
                     id2+=1
                 
-                # checks if clump2 is either shorter or longer than the other 
-                elif (((start_2-clump_error) <= start_1 <= (start_2+clump_error)) or 
-                     ((end_2 - clump_error) <= end_1 <= (end_2 + clump_error))):
-                    
-                    short_true = is_shorter(start_1, start_2, end_1, end_2, clump_error)
-                    
-                    # if clump 2 is shorter than clump 1, check if it is just a
-                    # split up version of clump 1
-                    if short_true:
+                # checks if clump 2 shorter than clump 1
+                elif is_shorter(start_1, start_2, end_1, end_2, clump_error):
                         
-                        clumps_within = check_split(end_1, uvb2["interval_end"], id2, clump_error)
-                        if clumps_within:
+                    clumps_within = check_split(end_1, uvb2["interval_end"], id2, clump_error)
+                    if clumps_within:
+                        
+                        split[id1] = []
+                        
+                        # puts current clump id and all of the clumps
+                        # contained within clump 1 bounds into split dict
+                        for clump in range(clumps_within):
                             
-                            split[id1] = []
-                            
-                            # puts current clump id and all of the clumps
-                            # contained within clump 1 bounds into split dict
-                            for clump in range(clumps_within+1):
-                                
-                                split[id1].append(id2+clump)
+                            split[id1].append(id2+clump)
 
-                            id1+=1
+                        id1+=1
+                        
+                        # accounting for current clump and all clumps that are
+                        # within clump 1
+                        id2+= (1 + clumps_within)
                             
-                            # accounting for current clump and all clumps that are
-                            # within clump 1
-                            id2+= (1 + clumps_within)
-                            
-                            
-                        else:
-                            shorter[id1] = id2
-                            id1+=1
-                            id2+=1    
-                            
-                    # if clump 2 is longer than clump 1, check if clump 2 is 
-                    # just a split up version of clump 1    
                     else:
+                        shorter[id1] = id2
+                        id1+=1
+                        id2+=1    
+                            
+                # checks if clump 2 is longer than clump 1
+                elif is_longer(start_1, start_2, end_1, end_2, clump_error):
+                    
+                    clumps_within = check_split(end_2, uvb1["interval_end"], id1, clump_error)
+                    if clumps_within:
                         
-                        clumps_within = check_split(end_1, uvb2["interval_end"], id2, clump_error)
-                        if clumps_within:
-                            
-                            merge[id2] = []
-                            
-                            # puts current clump id and all of the clumps
-                            # contained within clump 1 bounds into split dict
-                            for clump in range(clumps_within+1):
-                                
-                                merge[id2].append(id1+clump)
-                            
-                            # accounting for current clump and all clumps that are
-                            # within clump 1
-                            id1+= (1 + clumps_within)
-                            
-                            id2+=1
+                        merge[id2] = []
                         
-                        else:
-                            # clump 2 is longer than clump 1
-                            longer[id1] = id2
-                            id1+=1
-                            id2+=1
-                
+                        # puts current clump id and all of the clumps
+                        # contained within clump 1 bounds into split dict
+                        for clump in range(clumps_within):
+                            
+                            merge[id2].append(id1+clump)
+                        
+                        # accounting for current clump and all clumps that are
+                        # within clump 1
+                        id1+= (1 + clumps_within)
+                        
+                        id2+=1
+                    
+                    else:
+                        # clump 2 is longer than clump 1
+                        longer[id1] = id2
+                        id1+=1
+                        id2+=1
+
+                # checks for a rare overlapping case where a clump falls into neiter of
+                # the above categories
+                elif is_overlap(start_1, start_2, end_1, end_2, clump_error):
+                    
+                    overlap[id1] = id2
+                    id1+=1
+                    id2+=1 
+
                 else:
-                        
+                    # if the clumps do not fall into any other category, temorarily
+                    # store them in lonely and evaluate in the next iteration
                     lonely_1.append(id1)
                     
                     lonely_2.append(id2)
@@ -288,10 +371,112 @@ def pairwise_compare(salsa_out, ion_list, nrays):
                     id2+=1
             
             # creating list of sorted catagories         
-            sorted_list = [match, shorter, longer, split, merge, lonely_1, lonely_2]
+            sorted_list = [match, shorter, longer, split, merge, lonely_1, lonely_2, overlap]
             
             # storing list into output dict
             compare_dict[ion][ray] = sorted_list
     
     return compare_dict, col_dens_1, col_dens_2
+
+def get_true_rs(val): ##define how to get actual rshift numbers
+    if val == 20:
+        true_rs = '2.0'
+    elif val == 18:
+        true_rs = '2.5'
+    return true_rs
+
+# reading in arguments
+parser = argparse.ArgumentParser(description = "Select cutoff and UVB for analysis")
+
+parser.add_argument('-uvb_path1', action='store', 
+                    required=False, dest='uvb1', 
+                    help='Path to UVB file')
+
+parser.add_argument('-uvb_name1', action='store', 
+                    required=False, dest='uvb_name1', 
+                    help='Label to assign to uvb.')
+
+parser.add_argument('-uvb_path2', action='store', 
+                    required=False, dest='uvb2', 
+                    help='Path to UVB file')
+
+parser.add_argument('-uvb_name2', action='store', 
+                    required=False, dest='uvb_name2', 
+                    help='Label to assign to uvb.')
+
+args = parser.parse_args()
+dic_args = vars(args)
+
+uvb_names = [args.uvb_name1, args.uvb_name2]
+uvb_paths = [args.uvb1,args.uvb2]
+
+sal_args = configparser.ConfigParser()
+sal_args.read("./sal_params.par")
+
+# set desired halo pattern
+halo = sal_args["galaxy_settings"]["gal_pattern"]
+rs = sal_args["galaxy_settings"]["redshift"]
+nrays = int(sal_args["galaxy_settings"]["nrays"])
+
+# gets the true rs needed
+true_rs = get_true_rs(int(rs))
+
+# identifying the path argument as a variable
+out_file = sal_args["base_settings"]["output_file"]
+path = os.path.expandvars(os.path.expanduser(out_file))
+halo_path = path+'/halo'+f'{halo}'
+rs_path = halo_path + '/redshift'+f'{true_rs}'
+
+sal_dat = None
+
+nuvb = 2
+for i in range(nuvb):
+    dat_path = rs_path +f'/{uvb_names[i]}/data'
+    stat_path = rs_path +f'/{uvb_names[i]}/stats'
+
+    if i == 0:
+        with open(f'{dat_path}/salsa_out_dict.pickle',"rb") as dat:
+            sal_dat = pickle.load(dat)
+    else:
+        with open(f'{dat_path}/salsa_out_dict.pickle',"rb") as dat:
+            uvb_dict = pickle.load(dat)
+
+            for ion in uvb_dict.keys():
+        
+                sal_dat[ion][uvb_names[i]] = uvb_dict[ion][uvb_names[i]]
+
+
+print("Starting comparison", flush=True)
+compare_dict, col_dens_1, col_dens_2 = pairwise_compare(sal_dat, nrays)
+
+print("Starting condensing", flush=True)
+
+ion_list = sal_args["galaxy_settings"]["ions"].split(" ")
+
+uvb_dens_1 = {}
+uvb_dens_2 = {}
+dens_comp_dict = {}
+for ion in ion_list:
     
+    nion = ion.replace("_"," ")
+
+    uvb_dens_1[nion] = {}
+    uvb_dens_2[nion] = {}
+    dens_comp_dict[nion] = {}
+    for ray in range(nrays):
+        
+        uvb_dens_1[nion][ray], uvb_dens_2[nion][ray], dens_comp_dict[nion][ray] = condense_pairwise_data(compare_dict[nion][ray], 
+                                                                                                         sal_dat[nion], 
+                                                                                                         ray, 
+                                                                                                         nrays)
+
+out_dict = {uvb_names[0]:uvb_dens_1, uvb_names[1]:uvb_dens_2}
+
+out_file = open(f'{rs_path}/uvb_clump_labels_{uvb_names[0]}_{uvb_names[1]}.pickle',"wb") ##saves the dictonaries so that they can be accesssed later
+pickle.dump(compare_dict, out_file, protocol=3)	
+out_file.close()
+
+out_file = open(f'{rs_path}/uvb_compare_{uvb_names[0]}_{uvb_names[1]}.pickle',"wb") ##saves the dictonaries so that they can be accesssed later
+pickle.dump(out_dict, out_file, protocol=3)	
+out_file.close()
+
