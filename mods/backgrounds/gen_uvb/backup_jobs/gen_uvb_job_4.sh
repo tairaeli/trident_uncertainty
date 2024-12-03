effa9d4be162c77c9de431eae9db99abe97b138d
Elias Taira <tairaeli@msu.edu>
2024-12-03 10:39:33 -0500

    these should have been here a long time ago

---
+++ mods/backgrounds/gen_uvb/backup_jobs/gen_uvb_job_4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash --login
+
+# A template SLURM script for running CIAOLoop jobs in parallel
+
+#SBATCH -N 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 128
+#SBATCH --mem=200G
+#SBATCH -t 30:00:00
+#SBATCH -A galaxies
+#SBATCH --mail-user=tairaeli@msu.edu
+#SBATCH --mail-type=ALL
+
+# Do any job preamble stuff here; change directories, load modules, etc
+cd /mnt/home/tairaeli/astro_libs/cloudy_cooling_tools/
+
+# Run CIAOLoop in parallel
+./CIAOLoop -mp 4 4 -np 128 /mnt/home/tairaeli/trident_uncertainty/mods/backgrounds/gen_uvb/backup_jobs/uvb_params.par
