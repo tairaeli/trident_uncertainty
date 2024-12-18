f2f5e2fe55869bdb6484eaed273b70b652c73725
Elias Taira <tairaeli@msu.edu>
2024-12-18 11:17:08 -0500

    adding some comments

--- mods/backgrounds/uv_sal/pipeline/pair_comp_plot_paper.py
+++ mods/backgrounds/uv_sal/pipeline/pair_comp_plot_paper.py
@@ -1,8 +1,10 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from matplotlib.gridspec import GridSpec
 import seaborn as sns
+import pandas as pd
 import pickle
 import configparser
 import os
@@ -95,9 +97,11 @@ uvb_names = sal_args["uvb_analysis"]["uvb_names"].split(" ")
 
 sal_dat = None
 
-old_gen = [uvb_names[0], uvb_names[2], uvb_names[1]]
-new_gen = [uvb_names[1], uvb_names[3], uvb_names[3]]
+old_gen = [uvb_names[0], uvb_names[2], uvb_names[1]]#, uvb_names[0], uvb_names[2]]
+new_gen = [uvb_names[1], uvb_names[3], uvb_names[3]]#, uvb_names[3], uvb_names[1]]
 
+short_uvb_names = {"FG_2009":"FG09", "FG_2020":"FG20",
+                   "HM_2012":"HM12", "PCW_2019":"PW19"}
 
 for i,name in enumerate(uvb_names):
     dat_path = rs_path +f'/{name}/data'
@@ -114,6 +118,7 @@ for i,name in enumerate(uvb_names):
         
                 sal_dat[ion][name] = uvb_dict[ion][name]
 
+# loading in data
 ion_list = sal_args["galaxy_settings"]["ions"].split(" ")
 
 # # creating dictionaries to store our data if they don't already exist
@@ -123,75 +128,84 @@ if os.path.exists(uvb_dist_path) == False:
 
 # creating column density plots for each UVB
 num_ion = len(ion_list)
-fig, ax = plt.subplots((num_ion//2)+(num_ion%2),2, figsize=[8,8])
-i = 0
-j = 0
-for ion in sal_dat.keys():
-    for name in uvb_names:
-        sns.kdeplot(sal_dat[ion][name]["col_dens"], ax = ax[i,j], label = name)
-    ax[i,j].set_xlabel(r"$log_{10}$ $\sigma$ $cm^{-2}$")
-    ax[i,j].set_ylabel(r"Density")
-    ax[i,j].set_title(ion)
-    if (i == 0) and (j==0):
-        ax[i,j].legend()
 
-    if (j>0):
-        i+=1
-        j=0
-    else:
-        j+=1
-    
-plt.tight_layout()
-plt.grid()
-plt.savefig(uvb_dist_path+f"/dist_plot_paper.pdf")
-plt.clf()
+# reformatting ion labels
+ion_name_dict = {"H_I":r"H $\mathrm{\i}$",
+                 "Si_II":r"Si $\mathrm{\i\i}$",
+                 "Si_III":r"Si $\mathrm{\i\i\i}$",
+                 "C_III":r"C $\mathrm{\i\i\i}$",
+                 "Si_IV":r"Si $\mathrm{\i v}$",
+                 "C_IV":r"C $\mathrm{\i v}$",
+                 "N_V":r"N $\mathrm{v}$",
+                 "O_VI":r"O $\mathrm{v\i}$"}
 
-# making kde for density across entire ray
-fig, ax = plt.subplots((num_ion//2)+(num_ion%2),2, figsize=(10,10))
-colors = sns.color_palette("rocket",3)
-w_size = 10
+# setting up total column density figure
+fig, ax = plt.subplots(2, (num_ion//2)+(num_ion%2), figsize=(20,10))
+colors = sns.color_palette("rocket",len(old_gen))
+w_size = 20
+ion_txt_size = 30
 j=0
 k=0
+
 for ion in ion_list:    
     tot_ray_dens = {}
     n = len(str(nrays))
+
     for i in range(len(uvb_names)):
         tot_ray_dens[uvb_names[i]] = np.zeros(nrays)
 
     for ray in range(nrays):
         # loading in ray data
         n = len(str(nrays))
-        ray_dat = yt.load(rs_path+f"/rays/ray{ray:0{n}d}.h5")
-        ray_len = np.max(ray_dat.r[("gas","dl")].to("cm"))
+
+        if ion == "H_I":
+            ray_dat = yt.load(rs_path+f"/rays_HI/ray{ray:0{n}d}.h5")
+            ray_len = np.max(ray_dat.r[("gas","dl")].to("cm"))
+        
+        else:
+            ray_dat = yt.load(rs_path+f"/rays/ray{ray:0{n}d}.h5")
+            ray_len = np.max(ray_dat.r[("gas","dl")].to("cm"))
 
         for i in range(len(uvb_names)):
-            with open(rs_path +f'/{uvb_names[i]}/ray_dat/{ion}/ray_{ray:0{n}d}_dens.pickle', "rb") as dens_dat:
-                num_dens = pickle.load(dens_dat)
-            tot_ray_dens[uvb_names[i]][ray] = np.sum(num_dens*ray_len)
+            if ion == "H_I":
+                with open(rs_path +f'/{uvb_names[i]}/ray_dat/{ion}/ray_{ray:0{n}d}_dens_HI.pickle', "rb") as dens_dat:
+                    num_dens = pickle.load(dens_dat)
+                tot_ray_dens[uvb_names[i]][ray] = np.sum(num_dens*ray_len)
+            else:
+                with open(rs_path +f'/{uvb_names[i]}/ray_dat/{ion}/ray_{ray:0{n}d}_dens.pickle', "rb") as dens_dat:
+                    num_dens = pickle.load(dens_dat)
+                tot_ray_dens[uvb_names[i]][ray] = np.sum(num_dens*ray_len)
 
     for i in range(len(old_gen)):
         avg_dens = np.log10((tot_ray_dens[old_gen[i]] + tot_ray_dens[new_gen[i]])/2)
-        dens_diff = np.log10(tot_ray_dens[new_gen[i]]) - np.log10(tot_ray_dens[old_gen[i]])
+        dens_diff = np.log10(tot_ray_dens[old_gen[i]]) - np.log10(tot_ray_dens[new_gen[i]])
         
         moving_avg = np.convolve(dens_diff, np.ones(w_size) / w_size, mode='valid')
         x_vals = avg_dens[w_size-1:]
+        # adding ion label
+        if i == 0: 
 
-        sns.scatterplot(x = avg_dens, y = dens_diff, 
-                        label=f"{old_gen[i]}/{new_gen[i]}", 
-                        ax=ax[j,k], color = colors[i])
-        sns.lineplot(x = x_vals, y = moving_avg, ax=ax[j,k], color = colors[i])
-    ax[j,k].set_xlabel(r"log10(Avg Total Column Density)", fontsize=12)
-    ax[j,k].set_ylabel(r"log10($\frac{\sigma_{old}}{\sigma_{new}}$) ($cm^{-2}$)", fontsize=12)
-    ax[j,k].set_title(f"{ion} Total Column Density of {nrays} Rays", fontsize=15)
+            element,istate = ion.split("_")
+            ax[j,k].text(0.07, 0.7, 
+                  s=ion_name_dict[ion], fontsize=ion_txt_size,
+                  transform=ax[j,k].transAxes)
+            
+        plt.scatter(x = avg_dens, y = dens_diff, 
+                        label=f"{short_uvb_names[new_gen[i]]}/{short_uvb_names[old_gen[i]]}",
+                        color = colors[i])
+        plt.plot(x_vals, moving_avg, ax=ax[j,k], color = colors[i])
+
+    ax[j,k].set_xlabel(r"log($\overline{N}$) [$cm^{-2}$]", fontsize=12)
+    ax[j,k].set_ylabel(r"log($n_{old}/n_{new}$) [$cm^{-2}$]", fontsize=12)
     ax[j,k].grid()
 
-    if (k>0):
-        ax[j,k].legend()
+    if (k>=(num_ion//2)+(num_ion%2)-1):
         j+=1
         k=0
     else:
         k+=1
 
+ax[0,0].legend()
 plt.tight_layout()
 plt.savefig(uvb_dist_path+f"/tot_col_dens_comp.pdf", bbox_inches="tight")
 plt.clf()
